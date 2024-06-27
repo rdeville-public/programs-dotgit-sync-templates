33ae9c271338b768cc7c098764e599bfb5ac4085
Romain Deville <code@romaindeville.fr>
2024-06-27 23:30:22 +0200

    ✨ Add nixification of the repo

---
+++ modules/home-manager.nix
@@ -0,0 +1,17 @@
+# BEGIN DOTGIT-SYNC BLOCK MANAGED
+self: {
+  pkgs,
+  lib,
+# BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
+  ...
+}: {
+  xdg = {
+    configFile = {
+      dotfit-sync-templates = {
+        source = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      };
+    };
+  };
+# END DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
+}
+# END DOTGIT-SYNC BLOCK MANAGED
