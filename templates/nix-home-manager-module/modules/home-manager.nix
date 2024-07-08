3bbc3f818d771b46952469efc316124943f0cfb1
Romain Deville <code@romaindeville.fr>
2024-07-08 20:35:36 +0200

    🎨(templates): Update nix-HM-module example

--- templates/nix-home-manager-module/modules/home-manager.nix
+++ templates/nix-home-manager-module/modules/home-manager.nix
@@ -1,16 +1,36 @@
 self: {
   pkgs,
   lib,
-# BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
+  config,
+  # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
   ...
-}: {
+}: let
+  cfg = config.{{ slug }};
+in
+{
+  # TODO: Update hm module, below is exemple for enbale options
+  # options = {
+  #   {{ slug }} = {
+  #     enable = lib.mkEnableOption "TODO: Update description:";
+  #   };
+  # };
+
+  # config = lib.mkIf cfg.enable {
+  # TODO: Update hm module, below is exemple for a package
+  #   home = {
+  #     packages = [
+  #       self.packages.${pkgs.stdenv.hostPlatform.system}.default
+  #     ];
+  #   };
   # TODO: Update hm module, below is exemple for dotfiles
-  # xdg = {
-  #   configFile = {
-  #     {{ extra.repo.slug }} = {
-  #       source = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  #   xdg = {
+  #     configFile = {
+  #       {{ slug }} = {
+  #         source = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  #       };
   #     };
   #   };
   # };
-# END DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
+  # TODO: Update hm module, below is exemple for dotfiles
+  # END DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
 }
